ba81a66463c25219b08245f0c123fd4054bb5941
Munok <wer0222@naver.com>
2021-04-04 01:49:05 +0900

    백준 문제 10093번, 1267번

---
+++ MunokKim/AlgoStudy/AlgoStudy/BasicTips2/PrintStars4.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  AlgoStudy
+//
+//  Created by 김문옥 on 2021/03/28.
+//
+
+// https://www.acmicpc.net/problem/2441
+// Baekjoon 2441번 별 찍기 - 4
+
+// 문제
+// 첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
+// 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
+// 입력
+// 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+// 출력
+// 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+let totalLine: Int = Int(readLine()!)!
+
+for lineNumber in 0..<totalLine {
+    let starsCount: Int = totalLine - lineNumber
+    var lineString: String = ""
+    
+    for _ in 0..<lineNumber {
+        lineString = lineString + " "
+    }
+    
+    for _ in 0..<starsCount {
+        lineString = lineString + "*"
+    }
+    
+    print(lineString)
+}
