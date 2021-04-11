b224fa59d237440a1977392afdbd1121feb5888f
Munok <wer0222@naver.com>
2021-04-11 12:43:42 +0900

    5397번

--- MunokKim/AlgoStudy/AlgoStudy/LinkedList/main.swift
+++ MunokKim/AlgoStudy/AlgoStudy/LinkedList/main.swift
@@ -1,69 +1,62 @@
 //
-//  main.swift
+//  File.swift
 //  AlgoStudy
 //
-//  Created by 김문옥 on 2021/04/10.
+//  Created by 김문옥 on 2021/04/11.
 //
 
-// https://www.acmicpc.net/problem/1406
-// Baekjoon 1406번 에디터
+// https://www.acmicpc.net/problem/5397
+// Baekjoon 5397번 키로거
 
 //문제
-//한 줄로 된 간단한 에디터를 구현하려고 한다. 이 편집기는 영어 소문자만을 기록할 수 있는 편집기로, 최대 600,000글자까지 입력할 수 있다.
-//이 편집기에는 '커서'라는 것이 있는데, 커서는 문장의 맨 앞(첫 번째 문자의 왼쪽), 문장의 맨 뒤(마지막 문자의 오른쪽), 또는 문장 중간 임의의 곳(모든 연속된 두 문자 사이)에 위치할 수 있다. 즉 길이가 L인 문자열이 현재 편집기에 입력되어 있으면, 커서가 위치할 수 있는 곳은 L+1가지 경우가 있다.
-//이 편집기가 지원하는 명령어는 다음과 같다.
-//L    커서를 왼쪽으로 한 칸 옮김 (커서가 문장의 맨 앞이면 무시됨)
-//D    커서를 오른쪽으로 한 칸 옮김 (커서가 문장의 맨 뒤이면 무시됨)
-//B    커서 왼쪽에 있는 문자를 삭제함 (커서가 문장의 맨 앞이면 무시됨)
-//삭제로 인해 커서는 한 칸 왼쪽으로 이동한 것처럼 나타나지만, 실제로 커서의 오른쪽에 있던 문자는 그대로임
-//P $    $라는 문자를 커서 왼쪽에 추가함
-//초기에 편집기에 입력되어 있는 문자열이 주어지고, 그 이후 입력한 명령어가 차례로 주어졌을 때, 모든 명령어를 수행하고 난 후 편집기에 입력되어 있는 문자열을 구하는 프로그램을 작성하시오. 단, 명령어가 수행되기 전에 커서는 문장의 맨 뒤에 위치하고 있다고 한다.
+//창영이는 강산이의 비밀번호를 훔치기 위해서 강산이가 사용하는 컴퓨터에 키로거를 설치했다. 며칠을 기다린 끝에 창영이는 강산이가 비밀번호 창에 입력하는 글자를 얻어냈다.
+//키로거는 사용자가 키보드를 누른 명령을 모두 기록한다. 따라서, 강산이가 비밀번호를 입력할 때, 화살표나 백스페이스를 입력해도 정확한 비밀번호를 알아낼 수 있다.
+//강산이가 비밀번호 창에서 입력한 키가 주어졌을 때, 강산이의 비밀번호를 알아내는 프로그램을 작성하시오.
 //
 //입력
-//첫째 줄에는 초기에 편집기에 입력되어 있는 문자열이 주어진다. 이 문자열은 길이가 N이고, 영어 소문자로만 이루어져 있으며, 길이는 100,000을 넘지 않는다. 둘째 줄에는 입력할 명령어의 개수를 나타내는 정수 M(1 ≤ M ≤ 500,000)이 주어진다. 셋째 줄부터 M개의 줄에 걸쳐 입력할 명령어가 순서대로 주어진다. 명령어는 위의 네 가지 중 하나의 형태로만 주어진다.
-//dmih
-//11
-//B
-//B
-//P x
-//L
-//B
-//B
-//B
-//P y
-//D
-//D
-//P z
+//첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한줄로 이루어져 있고, 강산이가 입력한 순서대로 길이가 L인 문자열이 주어진다. (1 ≤ L의 길이 ≤ 1,000,000) 강산이가 백스페이스를 입력했다면, '-'가 주어진다. 이때 커서의 바로 앞에 글자가 존재한다면, 그 글자를 지운다. 화살표의 입력은 '<'와 '>'로 주어진다. 이때는 커서의 위치를 움직일 수 있다면, 왼쪽 또는 오른쪽으로 1만큼 움직인다. 나머지 문자는 비밀번호의 일부이다. 물론, 나중에 백스페이스를 통해서 지울 수는 있다. 만약 커서의 위치가 줄의 마지막이 아니라면, 커서 및 커서 오른쪽에 있는 모든 문자는 오른쪽으로 한 칸 이동한다.
+//2
+//<<BP<A>>Cd-
+//ThIsIsS3Cr3t
 //
 //출력
-//첫째 줄에 모든 명령어를 수행하고 난 후 편집기에 입력되어 있는 문자열을 출력한다.
-//yxz
+//각 테스트 케이스에 대해서, 강산이의 비밀번호를 출력한다. 비밀번호의 길이는 항상 0보다 크다.
+//BAPC
+//ThIsIsS3Cr3t
 
 import Foundation
 
-let inputText: String = readLine()!
-let commandCount: Int = Int(readLine()!)!
+let testCaseCount: Int = Int(readLine()!)!
+var testCases: [String] = []
 
-var leftText: String = inputText
-var rightText: String = ""
-
-for _ in 0..<commandCount {
-    var command: String = readLine()!
-    
-    switch command.first! {
-    case "L" where !leftText.isEmpty:
-        let cursoringCharacter: Character = leftText.removeLast()
-        rightText.append(cursoringCharacter)
-    case "D" where !rightText.isEmpty:
-        let cursoringCharacter: Character = rightText.removeLast()
-        leftText.append(cursoringCharacter)
-    case "B" where !leftText.isEmpty:
-        leftText.removeLast()
-    case "P":
-        let newCharacter: Character = command.removeLast()
-        leftText.append(newCharacter)
-    default: break
-    }
+for _ in 0..<testCaseCount {
+    testCases.append(readLine()!)
 }
 
-print(leftText + rightText.reversed())
+for testCase in testCases {
+    var leftText: String = ""
+    var rightText: String = ""
+    
+    for char in testCase {
+        switch char {
+        case "<":
+            if !leftText.isEmpty {
+                let cursoringChar: Character = leftText.removeLast()
+                rightText.append(cursoringChar)
+            }
+        case ">":
+            if !rightText.isEmpty {
+                let cursoringChar: Character = rightText.removeLast()
+                leftText.append(cursoringChar)
+            }
+        case "-":
+            if !leftText.isEmpty {
+                leftText.removeLast()
+            }
+        default:
+            leftText.append(char)
+        }
+    }
+    
+    print(leftText + rightText.reversed())
+}
