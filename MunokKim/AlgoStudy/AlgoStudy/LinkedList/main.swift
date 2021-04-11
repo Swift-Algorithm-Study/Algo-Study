d085e34d5b1b03981e434536ebd7ba97db066728
Munok <wer0222@naver.com>
2021-04-11 14:41:25 +0900

    1158번

--- MunokKim/AlgoStudy/AlgoStudy/LinkedList/main.swift
+++ MunokKim/AlgoStudy/AlgoStudy/LinkedList/main.swift
@@ -1,62 +1,47 @@
 //
-//  File.swift
+//  ㅡ먀ㅜ.swift
 //  AlgoStudy
 //
 //  Created by 김문옥 on 2021/04/11.
 //
 
-// https://www.acmicpc.net/problem/5397
-// Baekjoon 5397번 키로거
+// https://www.acmicpc.net/problem/1158
+// Baekjoon 1158번 요세푸스 문제
 
 //문제
-//창영이는 강산이의 비밀번호를 훔치기 위해서 강산이가 사용하는 컴퓨터에 키로거를 설치했다. 며칠을 기다린 끝에 창영이는 강산이가 비밀번호 창에 입력하는 글자를 얻어냈다.
-//키로거는 사용자가 키보드를 누른 명령을 모두 기록한다. 따라서, 강산이가 비밀번호를 입력할 때, 화살표나 백스페이스를 입력해도 정확한 비밀번호를 알아낼 수 있다.
-//강산이가 비밀번호 창에서 입력한 키가 주어졌을 때, 강산이의 비밀번호를 알아내는 프로그램을 작성하시오.
+//요세푸스 문제는 다음과 같다.
+//1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 이제 순서대로 K번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다. 예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+//N과 K가 주어지면 (N, K)-요세푸스 순열을 구하는 프로그램을 작성하시오.
 //
 //입력
-//첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한줄로 이루어져 있고, 강산이가 입력한 순서대로 길이가 L인 문자열이 주어진다. (1 ≤ L의 길이 ≤ 1,000,000) 강산이가 백스페이스를 입력했다면, '-'가 주어진다. 이때 커서의 바로 앞에 글자가 존재한다면, 그 글자를 지운다. 화살표의 입력은 '<'와 '>'로 주어진다. 이때는 커서의 위치를 움직일 수 있다면, 왼쪽 또는 오른쪽으로 1만큼 움직인다. 나머지 문자는 비밀번호의 일부이다. 물론, 나중에 백스페이스를 통해서 지울 수는 있다. 만약 커서의 위치가 줄의 마지막이 아니라면, 커서 및 커서 오른쪽에 있는 모든 문자는 오른쪽으로 한 칸 이동한다.
-//2
-//<<BP<A>>Cd-
-//ThIsIsS3Cr3t
+//첫째 줄에 N과 K가 빈 칸을 사이에 두고 순서대로 주어진다. (1 ≤ K ≤ N ≤ 5,000)
+//7 3
 //
 //출력
-//각 테스트 케이스에 대해서, 강산이의 비밀번호를 출력한다. 비밀번호의 길이는 항상 0보다 크다.
-//BAPC
-//ThIsIsS3Cr3t
+//예제와 같이 요세푸스 순열을 출력한다.
+//<3, 6, 2, 7, 5, 1, 4>
 
 import Foundation
 
-let testCaseCount: Int = Int(readLine()!)!
-var testCases: [String] = []
+let inputs: [String] = readLine()!
+    .components(separatedBy: " ")
 
-for _ in 0..<testCaseCount {
-    testCases.append(readLine()!)
-}
+let N: Int = Int(inputs.first!)!
+let K: Int = Int(inputs.last!)!
+var josephusPermutation: [Int] = Array(1...N)
+var rotator: Int = 1
+var output: String = ""
 
-for testCase in testCases {
-    var leftText: String = ""
-    var rightText: String = ""
-    
-    for char in testCase {
-        switch char {
-        case "<":
-            if !leftText.isEmpty {
-                let cursoringChar: Character = leftText.removeLast()
-                rightText.append(cursoringChar)
-            }
-        case ">":
-            if !rightText.isEmpty {
-                let cursoringChar: Character = rightText.removeLast()
-                leftText.append(cursoringChar)
-            }
-        case "-":
-            if !leftText.isEmpty {
-                leftText.removeLast()
-            }
-        default:
-            leftText.append(char)
-        }
+while (!josephusPermutation.isEmpty) {
+    if rotator == K {
+        let personForRemove: Int = josephusPermutation.removeFirst()
+        output.append("\(personForRemove)" + ", ")
+        rotator = 1
+    } else {
+        josephusPermutation.append(josephusPermutation.removeFirst())
+        rotator = rotator + 1
     }
-    
-    print(leftText + rightText.reversed())
 }
+
+output.removeLast(2)
+print("<" + output + ">")
