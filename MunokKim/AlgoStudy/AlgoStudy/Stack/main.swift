c732e84e2121ccc0bf2cfd5b04016b678a34ad4a
Munok <wer0222@naver.com>
2021-04-19 02:22:14 +0900

    1874번 문제

--- MunokKim/AlgoStudy/AlgoStudy/Stack/main.swift
+++ MunokKim/AlgoStudy/AlgoStudy/Stack/main.swift
@@ -1,46 +1,92 @@
 //
-//  File.swift
+//  main.swift
 //  AlgoStudy
 //
 //  Created by 김문옥 on 2021/04/18.
 //
 
-// https://www.acmicpc.net/problem/10773
-// Baekjoon 10773번 제로
+// https://www.acmicpc.net/problem/1874
+// Baekjoon 1874번 스택 수열
 
 //문제
-//나코더 기장 재민이는 동아리 회식을 준비하기 위해서 장부를 관리하는 중이다.
-//재현이는 재민이를 도와서 돈을 관리하는 중인데, 애석하게도 항상 정신없는 재현이는 돈을 실수로 잘못 부르는 사고를 치기 일쑤였다.
-//재현이는 잘못된 수를 부를 때마다 0을 외쳐서, 가장 최근에 재민이가 쓴 수를 지우게 시킨다.
-//재민이는 이렇게 모든 수를 받아 적은 후 그 수의 합을 알고 싶어 한다. 재민이를 도와주자!
+//스택 (stack)은 기본적인 자료구조 중 하나로, 컴퓨터 프로그램을 작성할 때 자주 이용되는 개념이다. 스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 제일 나중에 들어간 자료가 제일 먼저 나오는 (LIFO, Last in First out) 특성을 가지고 있다.
+//1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다. 이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다. 이를 계산하는 프로그램을 작성하라.
 
 //입력
-//첫 번째 줄에 정수 K가 주어진다. (1 ≤ K ≤ 100,000)
-//이후 K개의 줄에 정수가 1개씩 주어진다. 정수는 0에서 1,000,000 사이의 값을 가지며, 정수가 "0" 일 경우에는 가장 최근에 쓴 수를 지우고, 아닐 경우 해당 수를 쓴다.
-//정수가 "0"일 경우에 지울 수 있는 수가 있음을 보장할 수 있다.
+//첫 줄에 n (1 ≤ n ≤ 100,000)이 주어진다. 둘째 줄부터 n개의 줄에는 수열을 이루는 1이상 n이하의 정수가 하나씩 순서대로 주어진다. 물론 같은 정수가 두 번 나오는 일은 없다.
+//8
 //4
 //3
-//0
-//4
-//0
+//6
+//8
+//7
+//5
+//2
+//1
 
 //출력
-//재민이가 최종적으로 적어 낸 수의 합을 출력한다. 최종적으로 적어낸 수의 합은 231-1보다 작거나 같은 정수이다.
-//0
+//입력된 수열을 만들기 위해 필요한 연산을 한 줄에 한 개씩 출력한다. push연산은 +로, pop 연산은 -로 표현하도록 한다. 불가능한 경우 NO를 출력한다.
+//+
+//+
+//+
+//+
+//-
+//-
+//+
+//+
+//-
+//+
+//+
+//-
+//-
+//-
+//-
+//-
 
 import Foundation
 
+let sequenceCount: Int = Int(readLine()!)!
 var stack: Stack<Int> = Stack<Int>()
-let moneyCount: Int = Int(readLine()!)!
+var increaser: Int = 1
+var sequence: [Int] = []
+var output: [String] = []
+var lastNum: Int?
+var isNo: Bool = false
 
-for _ in 0..<moneyCount {
-    let money: Int = Int(readLine()!)!
-    
-    if money == 0 {
-        stack.pop()
-    } else {
-        stack.push(money)
-    }
+for _ in 0..<sequenceCount {
+    sequence.append(Int(readLine()!)!)
 }
 
-print(stack.items.reduce(0, +))
+for num in sequence {
+    if let topOfStack = stack.items.last {
+        if topOfStack == num {
+            stack.pop()
+            output.append("-")
+        } else if topOfStack > num {
+            isNo = true
+            break
+        }
+    }
+    
+    while increaser <= num {
+        stack.push(increaser)
+        output.append("+")
+        
+        if increaser == num {
+            stack.pop()
+            output.append("-")
+        }
+        
+        increaser = increaser + 1
+    }
+    
+    lastNum = num
+}
+
+if isNo {
+    print("NO")
+} else {
+    for _output in output {
+        print(_output)
+    }
+}
