c35cdaf59501ce5d5b86a56d00e510b283af0da5
민성홍 <sunghong32@naver.com>
2021-05-06 12:23:56 +0900

    테스트 커밋

---
+++ SungHongMIN/SungHongMIN/SungHongMIN/1406.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SungHongMIN
+//
+//  Created by 민성홍 on 2021/05/06.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
---
+++ SungHongMIN/SungHongMIN/SungHongMIN/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  SungHongMIN
+//
+//  Created by 민성홍 on 2021/05/06.
+//
+
+import Foundation
