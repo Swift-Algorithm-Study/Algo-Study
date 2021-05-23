4a95c9b51e575231417a26510b937c7f818e1764
Munok <wer0222@naver.com>
2021-05-23 22:27:00 +0900

    10799번 정답

--- MunokKim/AlgoStudy/AlgoStudy/UseOfTheStack/main.swift
+++ MunokKim/AlgoStudy/AlgoStudy/UseOfTheStack/main.swift
@@ -5,37 +5,38 @@
 //  Created by 김문옥 on 2021/05/23.
 //
 
-// https://www.acmicpc.net/problem/4949
-// Baekjoon 4949번 균형잡힌 세상
+// https://www.acmicpc.net/problem/10799
+// Baekjoon 10799번 쇠막대기
 
-var outputs: [Bool] = []
+// 레이저: |
+//
+// (((|(||)) (|)|)) (||)
+//     -||-
+//   -|-||-- -|-
+//  --|-||----|-|-
+// ---|-||----|-|-- -||-
+//
+// => 24
+// => 1+1+1+3+1+4+4+1+3+2+1+1+1
 
-while let strings: String = readLine(), strings != "." {
-    var stack: [Character] = []
-    
-    for char in strings {
-        switch char {
-        case "(": stack.append(char)
-        case "[": stack.append(char)
-        case ")":
-            if let stackLast: Character = stack.last, stackLast == "(" {
-                stack.removeLast()
-            } else {
-                stack.append(char)
-            }
-        case "]":
-            if let stackLast: Character = stack.last, stackLast == "[" {
-                stack.removeLast()
-            } else {
-                stack.append(char)
-            }
-        default: break
-        }
+import Foundation
+
+let input: String = readLine()!
+    .replacingOccurrences(of: "()", with: "|")
+var stack: [Character] = []
+var pieceCount: Int = 0
+
+for expression in input {
+    switch expression {
+    case "(":
+        stack.append(expression)
+        pieceCount += 1
+    case "|":
+        pieceCount += stack.count
+    case ")":
+        stack.removeLast()
+    default: break
     }
-    
-    outputs.append(stack.isEmpty)
 }
 
-for output in outputs {
-    print(output ? "yes" : "no")
-}
+print(pieceCount)
