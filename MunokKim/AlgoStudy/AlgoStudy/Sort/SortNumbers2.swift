680168f5951bdea7f48959220797ff2c4ced317f
Munok <wer0222@naver.com>
2021-06-27 23:21:19 +0900

    10989번 시간초과

--- MunokKim/AlgoStudy/AlgoStudy/Sort/SortNumbers2.swift
+++ MunokKim/AlgoStudy/AlgoStudy/Sort/SortNumbers2.swift
@@ -5,8 +5,8 @@
 //  Created by 김문옥 on 2021/06/27.
 //
 
-// https://www.acmicpc.net/problem/10989
-// Baekjoon 10989번 수 정렬하기 3
+// https://www.acmicpc.net/problem/2751
+// Baekjoon 2751번 수 정렬하기 2
 
 let numberOfNumber: Int = Int(readLine()!)!
 var outputs: [Int] = []
