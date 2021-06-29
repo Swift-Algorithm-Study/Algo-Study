b2ef5ba2dd07fa8f3cef6fefd6992428cf659216
Munok <wer0222@naver.com>
2021-06-29 21:15:25 +0900

    11651번 정답

--- MunokKim/AlgoStudy/AlgoStudy/Sort/SortCoordinate.swift
+++ MunokKim/AlgoStudy/AlgoStudy/Sort/SortCoordinate.swift
@@ -1,12 +1,12 @@
 //
-//  File.swift
+//  main.swift
 //  AlgoStudy
 //
-//  Created by 김문옥 on 2021/06/29.
+//  Created by 김문옥 on 2021/06/28.
 //
 
-// https://www.acmicpc.net/problem/11651
-// Baekjoon 11651번 좌표 정렬하기 2
+// https://www.acmicpc.net/problem/11650
+// Baekjoon 11650번 좌표 정렬하기
 
 let numberOfDots: Int = Int(readLine()!)!
 var coordinates: [(x: Int, y: Int)] = []
@@ -19,7 +19,7 @@ for _ in 0..<numberOfDots {
     coordinates.append(coordinate)
 }
 
-coordinates.sort { $0.y == $1.y ? $0.x < $1.x : $0.y < $1.y }
+coordinates.sort { $0.x == $1.x ? $0.y < $1.y : $0.x < $1.x }
 
 for coordinate in coordinates {
     print("\(coordinate.x) \(coordinate.y)")
